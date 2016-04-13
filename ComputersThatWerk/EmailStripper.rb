adde039c4faa7a92e35e3e3681cee06a4d29e6d6
Joe Preston <jpreston86@gmail.com>
2016-04-12 23:28:39 -0400

    Update EmailStripper.rb
    Fixed "file not found" issue.
    Added ability for user to name output file.
    Commented each step.

--- ComputersThatWerk/EmailStripper.rb
+++ ComputersThatWerk/EmailStripper.rb
@@ -1,16 +1,26 @@
-#Ruby Email Stripper
+################################################
+#    Ruby Email Stripper
+#    Created by: Joseph Preston
+#    Created on: 04.12.2016
+################################################
 
-output = 'emails.csv'
-
+#  Allows user to enter file location
 puts 'Enter your file location.'
 
-file = gets.strip.chomp()
+#  Strips leading and trailing whitespace and removes new line from file location
+filename = gets.strip.chomp()
 
-puts file
-puts File.exists?('"' + file + '"')
+#  Reads the contents of the file into memory
+@from = File.read(filename)
 
-@from = File.read('"' + file + '"')
-
+#  Scans the contents for REGEX defined email address, adds them to an array and removes any possible duplicates
 emails = Array(@from.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i)).uniq
 
+#  Allows the user to input a filename to export to
+puts 'Enter the name you wish the file to be.'
+
+#  Strips leading and trailing whitespace and removes new line from export filename
+output = gets.strip.chomp() + '.csv'
+
+#  Creates file with given export filename under the current working directory
 open(output, 'w') { |f| f.puts emails * "," }
